538b7f5353f064ae561e22b500a8be5fc5317914
Maria <araujomaria84@gmail.com>
2023-06-01 10:02:01 +0200

    Add Hero component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import '../src/css/main.css';
 import foto from '../src/images/foto.JPG';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Hero from './pages/Hero';
 
 function App() {
     return (
@@ -11,38 +12,9 @@ function App() {
                 {/*NAVBAR */}
                 <Navbar/>
 
-                {/* <!-- DESCRIPCION --> */}
-                    <div id="home" className="row py-3 text-white min-vh-100 align-items-center flex-wrap m-0">
+                {/* <!-- HERO --> */}
 
-                    {/* <!-- TITULO--> */}
-                    <div className="text-center">
-
-                        <h1 className="display-3">María Araujo Gil</h1>
-                        {/* <!-- FRASE --> */}
-                        <blockquote className="blockquote fs-6">
-                            <p>Una cita conocida, contenida en un elemento blockquote.</p>
-                        </blockquote>
-                        {/* <!-- LINKS --> */}
-                        <div className="p-5 icons d-flex justify-content-evenly">
-                            <a  href="https://github.com/maragil"
-                                target="_blank" rel="noopener noreferrer">
-                                <i className="fab fa-github fa-3x socialicons text-white"></i>
-                            </a>
-                            <a  href="https://www.linkedin.com/feed/"
-                                target="_blank" rel="noopener noreferrer">
-                                <i className="fab fa-linkedin fa-3x socialicons text-white"></i>
-                            </a>
-                            <a  href="http://"
-                                target="_blank" rel="noopener noreferrer">
-                                <i className="fab fa-twitter fa-3x socialicons text-white"></i>
-                            </a>
-                        </div>
-                        {/* <!-- BOTÓN --> */}
-                        <a className="btn btn-outline-light btn-lg"
-                            aria-label="Más acerca de mi"
-                            href="#aboutme">Más sobre mi</a>
-                    </div>
-                </div>
+                <Hero/>
 
     </header>
 
---
+++ src/pages/Hero.js
@@ -0,0 +1,39 @@
+function Hero () {
+
+    return (
+        <div id="home" className="row py-3 text-white min-vh-100 align-items-center flex-wrap m-0">
+
+            {/* <!-- NOMBRE--> */}
+            <div className="text-center">
+                <h1 className="display-3">María Araujo Gil</h1>
+
+                {/* <!-- CITA--> */}
+                <blockquote className="blockquote fs-6">
+                    <p>Una cita conocida, contenida en un elemento blockquote.</p>
+                </blockquote>
+
+                {/* <!-- LINKS --> */}
+                <div className="p-5 icons d-flex justify-content-evenly">
+                    <a  href="https://github.com/maragil"
+                        target="_blank">
+                        <i className="fab fa-github fa-3x socialicons text-white"></i></a>
+
+                    <a  href="https://www.linkedin.com/feed/"
+                        target="_blank">
+                        <i className="fab fa-linkedin fa-3x socialicons text-white"></i></a>
+
+                    <a  href="http://"
+                        target="_blank">
+                        <i className="fab fa-twitter fa-3x socialicons text-white"></i></a>
+                </div>
+
+                {/* <!-- BOTÓN --> */}
+                <a className="btn btn-outline-light btn-lg"
+                    aria-label="Más acerca de mi"
+                    href="#aboutme">Más sobre mi</a>
+            </div>
+        </div>
+    )
+}
+
+export default Hero;
