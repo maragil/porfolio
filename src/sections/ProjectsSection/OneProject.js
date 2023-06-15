76e8d3dc277518e32f73f5b70c07186f590d3ada
Maria <araujomaria84@gmail.com>
2023-06-15 21:34:10 +0200

    Add changes in projects cards

--- src/sections/ProjectsSection/OneProject.js
+++ src/sections/ProjectsSection/OneProject.js
@@ -3,20 +3,24 @@ function OneProject ({eachProject}){
     return (
 
         <div className="col-md-5 card mt-3">
-            <div className="card-body-overlay mt-2">
+            <div className="card-body-overlay mt-2 d-flex flex-column">
                 <h5 className="card-title text-center">
+                    
                     <a href={eachProject.url}
                         className="text-reset"
                         target= "_blank"> {eachProject.name}</a>
+                        
                 </h5>
                 <p className="card-text">{eachProject.desc}</p>
-                <p>{eachProject.tec}</p>
-                <div className="link-git text-center">
-                    <a href={eachProject.repo}
+                <p className="d-flex flex-row justify-content-around gap-3">{eachProject.tec}
+                <a href={eachProject.repo}
                     target="_blank">
                         <i className="fab fa-github text-dark"></i>
-                    </a>
-                </div>
+                </a>
+                </p>
+                {/* <div className="link-git text-center">
+                   
+                </div> */}
             </div>
         </div>
     )
