bb2b9f34027ee32c016326a89e4d8224277f061f
Maria <araujomaria84@gmail.com>
2023-07-24 19:17:33 +0200

    Changes in Recaptcha.js

--- src/components/Recaptcha.js
+++ src/components/Recaptcha.js
@@ -1,16 +1,22 @@
+import { useRef } from "react";
 import ReCAPTCHA from "react-google-recaptcha";
 
 function Recaptcha () {
 
+    const captcha = useRef(null);
+
     const onChange = () => {
-        console.log('hubo un cambio');
+
+        if(captcha.current.getValue()){
+            console.log('El usuario no es un robot')
+        }
     }
 
     return (
 
         <div className="recaptcha">
             <ReCAPTCHA
-            className="rec"
+                ref={captcha}
                 sitekey="6LcAf04nAAAAAFRxQvFAKrc6vOliSvZohxMtVSc2"
                 onChange={onChange}
             />
